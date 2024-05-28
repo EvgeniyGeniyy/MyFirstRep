fb77e2d7473be33403b3f6889a523551c5c61a2c
EvgeniyGeniy <polyakovskiy1996@mail.ru>
2024-05-28 15:15:23 +0700

    Урок 12

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -1,39 +1,80 @@
 package com.example.test2
 
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import com.example.test2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tv : TextView
-    lateinit var bt : Button
-
+    lateinit var bindingClass : ActivityMainBinding
 
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
-        setContentView(R.layout.activity_main)
-        tv = findViewById(R.id.tvTest)
-        bt = findViewById(R.id.buttonTest)
-        bt.setOnClickListener {
-            tv.text = "It is Work!"
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        bindingClass.btRezult.setOnClickListener {
+
+            val resultValue = bindingClass.edName.text.toString()
+
+            when(resultValue){
+                Constance.DIRECTOR ->{
+                    bindingClass.textView.visibility = View.VISIBLE
+                    val tText = "Получите зарплату в размере ${Constance.DIRECTOR_SUM} рублей"
+                    if(bindingClass.edPass.text.toString() == Constance.DIIRECTOR_PASSWORD){
+                    bindingClass.textView.text = tText
+                    } else {
+                        bindingClass.textView.text = "Неверный пороль!"
+                    }
+                }
+                Constance.INGENER ->{
+                    bindingClass.textView.visibility = View.VISIBLE
+                    val tText = "Получите зарплату в размере ${Constance.INGENER_SUM} рублей"
+                    if(bindingClass.edPass.text.toString() == Constance.IGENER_PASSWORD){
+                        bindingClass.textView.text = tText
+                    } else {
+                        bindingClass.textView.text = "Неверный пороль!"
+                    }
+                }
+                Constance.DVORNIK ->{
+                bindingClass.textView.visibility = View.VISIBLE
+                val tText = "Получите зарплату в размере ${Constance.DVORNIK_SUM} рублей"
+                    if(bindingClass.edPass.text.toString() == Constance.DVORNIK_PASSWORD){
+                        bindingClass.textView.text = tText
+                    } else {
+                        bindingClass.textView.text = "Неверный пороль!"
+                    }
+            }
+                else -> {
+
+                    bindingClass.textView.visibility = View.VISIBLE
+                    bindingClass.textView.text ="Такой сотрудник не найден!"
+
+                }
+
+
+
+
+            }
         }
-    }
-
-    fun onClickTest(view: View){
-
 
 
     }
+
+    object Constance{
+        const val INGENER_SUM = 10000
+        const val DVORNIK_SUM = 1000
+        const val DIRECTOR_SUM = 100000
+
+        const val DIRECTOR = "Андрей"
+        const val INGENER = "Эдуард"
+        const val DVORNIK = "Егор"
+
+        const val DVORNIK_PASSWORD = "4455"
+        const val IGENER_PASSWORD = "2233"
+        const val DIIRECTOR_PASSWORD = "1234"
+
+    }
+
 
 }
