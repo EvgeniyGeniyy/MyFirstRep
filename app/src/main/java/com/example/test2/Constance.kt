59401c4f6c89c1934d12c11b72a85773762f31e0
EvgeniyGeniy <polyakovskiy1996@mail.ru>
2024-05-31 20:27:29 +0700

    Урок16 Переход между активити

---
+++ app/src/main/java/com/example/test2/Constance.kt
@@ -0,0 +1,17 @@
+package com.example.test2
+
+object Constance {
+
+    const val INGENER_SUM = 10000
+    const val DVORNIK_SUM = 1000
+    const val DIRECTOR_SUM = 100000
+
+    const val DIRECTOR = "Андрей"
+    const val INGENER = "Эдуард"
+    const val DVORNIK = "Егор"
+
+    const val DVORNIK_PASSWORD = "4455"
+    const val IGENER_PASSWORD = "2233"
+    const val DIIRECTOR_PASSWORD = "1234"
+
+}
---
+++ app/src/main/res/TestActivity2.kt
@@ -0,0 +1,2 @@
+class TestActivity2 {
+}
